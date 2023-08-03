9390a922f9a6e4312e6407e9f4f7bd13a73049b8
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2023-08-03 19:25:47 +0545

    Program to implement Binary Search Tree in C
    Program to implement Binary Search Tree in C

---
+++ BST.c
@@ -0,0 +1,101 @@
+//WAP to implement Binary Search Tree in C
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure of a tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Function to insert a new node into the BST
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    } else if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+// Function to perform Preorder traversal (Root -> Left -> Right)
+void preorderTraversal(struct Node* root) {
+    if (root == NULL) {
+        return;
+    }
+    printf("%d ", root->data);
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+// Function to perform Inorder traversal (Left -> Root -> Right)
+void inorderTraversal(struct Node* root) {
+    if (root == NULL) {
+        return;
+    }
+    inorderTraversal(root->left);
+    printf("%d ", root->data);
+    inorderTraversal(root->right);
+}
+
+// Function to perform Postorder traversal (Left -> Right -> Root)
+void postorderTraversal(struct Node* root) {
+    if (root == NULL) {
+        return;
+    }
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    printf("%d ", root->data);
+}
+
+// Function to free the memory used by the BST
+void freeBST(struct Node* root) {
+    if (root != NULL) {
+        freeBST(root->left);
+        freeBST(root->right);
+        free(root);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+
+    // Insert nodes into the BST
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    // Perform traversals
+    printf("Preorder Traversal: ");
+    preorderTraversal(root);
+    printf("\n");
+
+    printf("Inorder Traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    printf("Postorder Traversal: ");
+    postorderTraversal(root);
+    printf("\n");
+
+    // Free the memory used by the BST
+    freeBST(root);
+
+    return 0;
+}
